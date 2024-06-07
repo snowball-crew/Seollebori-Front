e9c5b34d2c62b53998ebe4f89811bb42f813cce7
Lami <Halam.Lee.Lami@gmail.com>
2024-06-07 09:19:45 +0900

    fix: 빌드 설정 파일 수정

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ../
 mkdir output
-cp -R ./snowball-crew/Seollebori-Front/* ./output
-cp -R ./output ./snowball-crew/Seollebori-Front/
+cp -R ./Seollebori-Front/* ./output
+cp -R ./output ./Seollebori-Front/
